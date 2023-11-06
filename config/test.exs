a6b6042322ac999e18ae347625151ec3e603862d
Jordi <jordi.bonet.valiente@lambdaclass.com>
2023-11-06 11:22:18 +0100

    First commit

---
+++ config/test.exs
@@ -0,0 +1,27 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :chat, Chat.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "chat_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :chat, ChatWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "4EK2t7615RMA8XYhRU/OoUq5WiR7L2Z2uObPBbBoE1gKCPAOE3Qij9JuQpk8WBSv",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
